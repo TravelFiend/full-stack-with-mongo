0c5ca19f536372cb953191f6dadec53b260fea46
TravelFiend <mikegrace274@gmail.com>
2020-01-10 00:16:13 -0800

    userSignUp component written

--- public/auth-page/UserSignUp.js
+++ public/auth-page/UserSignUp.js
@@ -1,27 +1,59 @@
 import Component from '../Component.js';
 
 class UserSignUp extends Component {
+    onRender(form){
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            const user = {
+                username: formData.get('username'),
+                email: formData.get('username'),
+                password: formData.get('password')
+            };
+
+            fetch('/api/v1/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+                .then(res => res.json())
+                .then(user => {
+                    if(user._id){
+                        window.location.href = '../mypages/my-pages.js';
+                    }
+                    const error = document.createElement('div');
+                    error.textContent = 'Incorrect email or password';
+                    form.appendChild(error);
+                    if(form.children.length > 2) form.removeChild(form.lastChild);
+                });
+        });
+    }
+
     renderHTML(){
         return /*html*/`
-            <form id="sign-up-form">
-                <div>
-                    <label for="name"><label>
-                    <input class="user-input" id="name" name="name" placeholder="Your Name" />
-                </div>
+            <div>
+                <form id="sign-up-form">
+                    <div>
+                        <label for="name">Username:<label>
+                        <input class="user-input" id="username" name="username"/>
+                    </div>
 
-                <div>
-                    <label for="email"><label>
-                    <input class="user-input" id="email" name="email" type="email" placeholder="Email"/>
-                </div>
+                    <div>
+                        <label for="email">Email:<label>
+                        <input class="user-input" id="email" name="email" type="email"/>
+                    </div>
 
-                <div>
-                    <label for="password"><label>
-                    <input class="user-input" id="password" name="password" type="password" placeholder="Password"/>
-                </div>
+                    <div>
+                        <label for="password">Password:<label>
+                        <input class="user-input" id="password" name="password" type="password"/>
+                    </div>
 
-                <button class="auth-button">Create Account</button>
-                <button class="auth-button">Already have an account?</button>
-            </form>
+                    <button class="auth-button">Create Account</button>
+                </form>
+            </div>
         `;
     }
 }
